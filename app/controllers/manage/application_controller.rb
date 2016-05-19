8570d04d095a6d2ef065bcd76162638634b0d784
Evgeny Lapin <lev@openteam.ru>
2016-05-19 16:43:56 +0600

    added config for annotate

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -2,4 +2,12 @@ class Manage::ApplicationController < ApplicationController
 
   layout 'manage'
 
+  rescue_from CanCan::AccessDenied do |exception|
+    if user_signed_in?
+      redirect_to root_path, alert: 'Недостаточно прав для выполнения операции'
+    else
+      redirect_to sign_in_url, alert: 'Для доступа необходимо войти в систему'
+    end
+  end
+
 end
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,5 +1,7 @@
 class Permission < ActiveRecord::Base
+
   include AuthClient::Permission
 
   acts_as_auth_client_permission roles: [:admin]
+
 end
