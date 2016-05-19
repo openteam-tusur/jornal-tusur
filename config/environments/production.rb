41275776c59c0476488dbedfc98f58682c690229
Evgeny Lapin <lev@openteam.ru>
2016-05-19 10:25:15 +0600

    completle stub-app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailer
+  config.action_mailer.default_url_options = { host: Settings['app.host'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Settings['smtp']
 end
