d23abcddf4af1b7fafb7b06ebaf69f7424a6ec68
Evgeny Lapin <lev@openteam.ru>
2016-05-25 10:44:46 +0600

    require only autocomplete from jquery-ui

--- app/assets/javascripts/manage/application.js
+++ app/assets/javascripts/manage/application.js
@@ -4,7 +4,7 @@
  * = require turbolinks
  * = require bootstrap-sprockets
  * = require tusur_header
- * = require jquery-ui
+ * = require jquery-ui/autocomplete
  * = require ./user_autocomplete
  * = require ./article_form
  * = require ./main
