aa97a59fc4a1d87e280d421109ef14a6047730b0
Evgeny Lapin <lev@openteam.ru>
2016-06-02 16:39:48 +0700

    set ru locale in manage

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -2,6 +2,8 @@ class Manage::ApplicationController < ApplicationController
 
   layout 'manage'
 
+  before_filter :set_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     if user_signed_in?
       redirect_to root_path, alert: 'Недостаточно прав для выполнения операции'
@@ -9,5 +11,11 @@ class Manage::ApplicationController < ApplicationController
       redirect_to sign_in_url, alert: 'Для доступа необходимо войти в систему'
     end
   end
+
+  private
+
+    def set_locale
+      I18n.locale = :ru
+    end
 
 end
