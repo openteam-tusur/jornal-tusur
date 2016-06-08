b9dbf1c11e9bd7d9a8621c79d971f4d5c8842c8b
Evgeny Lapin <lev@openteam.ru>
2016-06-08 09:29:57 +0700

    removed unnided files

--- app/assets/javascripts/manage/article_form/app.coffee
+++
@@ -1,4 +0,0 @@
-angular.module('article_form', ['ngFileUpload', 'angucomplete-alt'])
-
-$(document).on 'ready page:load', ->
-  angular.bootstrap($('.js-article-form'), ['article_form']) if $('.js-article-form').length
--- app/assets/javascripts/manage/article_form/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * = require angular
- * = require ng-file-upload/ng-file-upload-shim
- * = require ng-file-upload/ng-file-upload
- * = require angucomplete-alt
- * = require ./app
- * = require ./article_authors
- * = require ./article_controller
- */
